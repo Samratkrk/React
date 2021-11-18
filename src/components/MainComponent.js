a9bba33b1e4543167be7e5a550d46705de63bbfa
Samrat Karki <samratkrk@outlook.com>
2021-11-18 14:16:14 -0500

    Adding to the Single page app

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,15 +3,23 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { CAMPSITES } from '../shared/campsites';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/campsites';
+import { PARTNERS } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/campsites';
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            campsites: CAMPSITES,        
+            campsites: CAMPSITES,     
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS,   
         };
     }
 
@@ -19,7 +27,11 @@ class Main extends Component {
         
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
             );
         };
 
@@ -29,7 +41,8 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
-                    <Redirect to='/home' />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to='/home' />               
                 </Switch>
                 <Footer />
             </div>

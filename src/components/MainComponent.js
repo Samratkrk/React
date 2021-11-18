6b3469af212edd70dd241976cbda5872738cb9ac
Samrat Karki <samratkrk@outlook.com>
2021-11-18 15:44:33 -0500

    React Router Parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -31,10 +31,18 @@ class Main extends Component {
                     campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
                     promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
                     partner={this.state.partners.filter(partner => partner.featured)[0]}
-                    
                 />
             );
         };
+       
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };  
 
         return (
             <div>
@@ -42,8 +50,9 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
-                    <Redirect to='/home' />               
+                    <Redirect to='/home' />             
                 </Switch>
                 <Footer />
             </div>
